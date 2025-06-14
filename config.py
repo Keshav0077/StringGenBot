06b56cf4922d1533736e7f2816ce2ccc32d44c61
Keshav0077 <keshavrajlkr4@gmail.com>
2025-06-14 16:50:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("26954495"))
+API_HASH = getenv("2061c55207cfee4f106ff0dc331fe3d9")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/JISSHU_BOTS")
+OWNER_ID = int(getenv("OWNER_ID", 7045947967))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/moviesbazaarsupp")
